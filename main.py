06d1d625e93ed0f5f956160cc9c260c0922935f0
Sahal <sahal@atliq.com>
2026-02-26 22:52:45 +0530

    initial commit 555

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ class MessageRequest(BaseModel):
 def health_check():
     return {"status": "ok"}
 
-# @app.get("/trial")
-# def health_checktrial():
-#     return {"status": "ok"}
+@app.get("/trial")
+def health2():
+    return {"status": "ok"}
 
 @app.post("/message")
 def post_message(body: MessageRequest):
