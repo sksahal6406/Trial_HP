2d5551d6f9efcd1e42bd04d2fe116bf0c56fc6d6
Sahal <sahal@atliq.com>
2026-02-27 12:55:52 +0530

    Trial For Demo

--- main.py
+++ main.py
@@ -8,13 +8,7 @@ class MessageRequest(BaseModel):
     message: str
 
 
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
 
-@app.get("/trial")
-def health2():
-    return {"status": "ok"}
 
 
 @app.post("/message")
